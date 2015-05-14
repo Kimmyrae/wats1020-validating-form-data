8b5141b43201dfc21f47d7fb3ee0df8c5ce3c42d
kimberly.mcbain <kimberly.mcbain@gmail.com>
2015-05-14 15:26:33 -0400

    added js

--- js/main.js
+++ js/main.js
@@ -12,11 +12,43 @@ $(document).on('ready', function(){
             form.submit();
         },
         rules: {
+			"your-name": {
+                required: true,
+                maxlength: 128
+            },
+            "your-state": {
+                required: true,
+                maxlength: 2
+            },
+            "your-zip": {
+                required: true,
+                maxlength: 5,
+                digits: true
+            },
+            "card-holder-name": {
+                required: true,
+                maxlength: 128
+            },
+            "card-number": {
+                required: true,
+                creditcard: true
+            },
+            "cvv": {
+                required: true,
+                maxlength: 3,
+                digits: true
 			
-		}
+			  }
+        }
+    });
+    // Tooltips
+    $('label span.glyphicon').tooltip();
+			
+			
+			
+});
 // 3. Connect the validation object to an event handler tied to the submit button.
 
 // Refer to the `index.html` file for the validation rules that must be enforced.
 		
-	});
-});
+	
